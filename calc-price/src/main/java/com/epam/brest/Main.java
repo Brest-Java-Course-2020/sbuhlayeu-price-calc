71b3bbbdffa3b3883b6405a9477fa85b966559a8
Stanislau Buhlayeu <workuniver901@gmail.com>
2020-02-02 16:53:08 -0800

    Intermediate result

--- calc-price/src/main/java/com/epam/brest/Main.java
+++ calc-price/src/main/java/com/epam/brest/Main.java
@@ -1,7 +1,9 @@
 package com.epam.brest;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("First maven-project");
+
+    public static void main(String[] args){
+        PriceCalc priceCalcObject = new PriceCalc();
+        priceCalcObject.priceCalculation();
     }
 }
---
+++ calc-price/src/main/java/com/epam/brest/PriceCalc.java
@@ -0,0 +1,59 @@
+package com.epam.brest;
+
+import java.util.Scanner;
+
+public class PriceCalc {
+    public void priceCalculation() {
+        Double[] enteredValues = new Double[4];
+        Scanner scanner = new Scanner(System.in);
+        String inputValue;
+        Double calcResult;
+
+        int i = 0;
+
+        do {
+            switch (i) {
+                case 0: System.out.println("Please, enter distance or Q for exit: "); break;
+                case 1: System.out.println("Please, enter price per km or Q for exit: "); break;
+                case 2: System.out.println("Please, enter weight or Q for exit: "); break;
+                case 4: {
+                    if (enteredValues[0] >= 1000 && enteredValues[2] < 1000) {
+                        calcResult = enteredValues[0] * (enteredValues[1] * 0.50) + enteredValues[2] * enteredValues[3];
+                    } else if (enteredValues[2] >= 1000 && enteredValues[0] < 1000) {
+                        calcResult = enteredValues[0] * enteredValues[1] + enteredValues[2] * (enteredValues[3] * 0.50);
+                    } else if (enteredValues[0] >= 1000 && enteredValues[2] >= 1000) {
+                        calcResult = enteredValues[0] * (enteredValues[1] * 0.50) + enteredValues[2] * (enteredValues[3] * 0.50);
+                    } else {
+                        calcResult = enteredValues[0] * enteredValues[1] + enteredValues[2] * enteredValues[3];
+                    }
+                    System.out.println("Price: $" + calcResult);
+                    i = 0;
+                }
+                default: System.out.println("Please, enter price per kg or Q for exit: ");
+            }
+
+            inputValue = scanner.next();
+            if (!isExitValue(inputValue) && isCorrectDoubleValue(inputValue)) {
+                enteredValues[i] = Double.parseDouble(inputValue);
+                i++;
+            }
+        } while (!isExitValue(inputValue));
+        System.out.println("Finish!");
+    }
+
+    private static boolean isExitValue(String value) {
+        return value.equalsIgnoreCase("Q");
+    }
+
+    private static boolean isCorrectDoubleValue(String value) {
+        boolean checkResult;
+        try {
+            double enteredDoubleValue = Double.parseDouble(value);
+            checkResult = enteredDoubleValue > 0;
+        } catch (NumberFormatException ex) {
+            checkResult = false;
+        }
+        return checkResult;
+    }
+}
+
